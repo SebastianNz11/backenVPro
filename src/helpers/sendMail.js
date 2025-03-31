9b762b6e903df4c63373883bb9f93336f3e3dc7d
Sebastian Nuñez <sebas11np@gmail.com>
2025-03-31 10:17:04 -0600

    commit1

---
+++ src/helpers/sendMail.js
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+export const sendEmail = async (nombre, email, mensaje) => {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.CORREO,
+      pass: process.env.CLAVEGOOGLE,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  console.log(email)
+  let info = await transporter.sendMail({
+    from: `${nombre} ${email}`,
+    replyTo: email,
+    to: process.env.CORREO,
+    subject: "Datos del formulario",
+    text: `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`,
+  });
+
+  console.log("Mensaje enviado: %s", info.messageId);
+};
